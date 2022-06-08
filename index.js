636e2426ba43a1c7a3a2f5436f960b40fa70135f
ainoi <muawatt@Watts.local>
2022-06-08 14:38:11 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+console.log("tets");
+const keysEle = document.querySelectorAll('.keys');
+const codesEle = document.querySelectorAll('.codes');
+
+
+console.log("keys", keysEle);
+console.log("codes", codesEle);
+
+for(let i=0; i<keysEle.length; i++) {
+    
+}
+
+window.addEventListener("keydown", (event) => {
+
+    
+    // for(let i=0; i<keysEle.length; i++) {
+
+    //     if((keysEle[i].innerText == event.key) || (keysEle[i].innerText.toLowerCase() == event.key)) {
+    //         console.log(event.key);
+    //     } 
+    //     else {
+            // keysEle[i].setAttribute("tempClass", event.code);
+            // console.log(keysEle[i]);
+            // console.log('elseeeee');
+        // }
+
+    // }
+});
