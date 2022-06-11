4c1ba6afb6ca30eb4a7be74b4716c9e50018cd76
ainoi <muawatt@gmail.com>
2022-06-12 00:50:41 +0700

    done

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ window.addEventListener("keydown", (event) => {
     for(let i=0; i<keysEle.length; i++) {
         if(((keysEle[i].innerText) === event.key) 
             || ((keysEle[i].innerText.toLowerCase()) === event.key)) {
-            keysEle[i].style.backgroundColor = "#808080";
+            keysEle[i].style.backgroundColor = "#ff0000";
             setTimeout(() => {
                keysEle[i].style.backgroundColor = "#FFFFFF";
-            }, 200);
+            }, 160);
         }
     }
     for(let i=0; i<codesEle.length; i++) {
         if((codesEle[i].getAttribute('tempclass')) === event.code) {
-            codesEle[i].style.backgroundColor = "#808080";
+            codesEle[i].style.backgroundColor = "#ff0000";
             setTimeout(() => {
                 codesEle[i].style.backgroundColor = "#FFFFFF";
-            }, 200);
+            }, 160);
         }
     }
 });
